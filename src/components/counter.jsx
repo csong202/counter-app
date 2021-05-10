b4e7fe0cb053aa1eabc88666616800b1e8e81dc4
csong202 <csong097@uottawa.ca>
2021-05-10 15:38:28 -0400

    implemented suggestions for counter (declared increment etc outside render,1 line getBadgeClasses)

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -22,27 +22,40 @@ class Counter extends Component {
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           id={this.getBtnIncrId(counter.id)}
-          onClick={() => this.props.onIncrement(this.props.counter)}
+          // onClick={() => this.props.onIncrement(this.props.counter)}
+          onClick={this.incrementCounter.bind(this)}
           disabled={this.isBtnIncrDis(counter, counters)}
           className="btn btn-secondary btn-sm"
         >Increment
         </button>
         <button 
           id={this.getBtnDecrId(counter.id)}
-          onClick={() => this.props.onDecrement(this.props.counter)}
+          onClick={this.decrementCounter.bind(this)}
           disabled={this.isBtnDecrDis(counter, counters)}
           className="btn btn-secondary btn-sm m-2"
         >Decrement
         </button>
         <button 
           id={this.getBtnDelId(counter.id)}
-          onClick={() => this.props.onDelete(this.props.counter.id)} 
+          onClick={this.deleteCounter.bind(this)}
           disabled={this.isBtnDelDis(counter, counters)}
           className="btn btn-danger btn-sm"
         >Delete
         </button>
       </div>
     );
+  }
+
+  incrementCounter() {
+    this.props.onIncrement(this.props.counter);
+  }
+
+  decrementCounter() {
+    this.props.onDecrement(this.props.counter);
+  }
+
+  deleteCounter() {
+    this.props.onDelete(this.props.counter.id);
   }
 
   getBtnIncrId(counterId) {
@@ -59,25 +72,24 @@ class Counter extends Component {
 
   isBtnIncrDis(counter, counters) {
     const index = counters.indexOf(counter);
-    console.log("isBtnIncrDis - ", counters[index].disabled);
+    // console.log("isBtnIncrDis - ", counters[index].disabled);
     return counters[index].disabled;
   }
 
   isBtnDecrDis(counter, counters) {
     const index = counters.indexOf(counter);
-    console.log("isBtnDecrDis - ", counters[index].disabled);
+    // console.log("isBtnDecrDis - ", counters[index].disabled);
     return counters[index].disabled;
   }
 
   isBtnDelDis(counter, counters) {
     const index = counters.indexOf(counter);
-    console.log("isBtnDelDis - ", counters[index].disabled);
+    // console.log("isBtnDelDis - ", counters[index].disabled);
     return counters[index].disabled;
   }
 
   getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    const classes = `badge m-2 badge-${this.props.counter.value === 0 ? "warning" : "primary"}`;
     // console.log("badge class", classes);
     return classes;
   }
